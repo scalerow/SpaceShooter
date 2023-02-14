2f0f9ddf06ba86afcba7257be6e8abee770b9e80
Jan Fredrik Sundblom <jansun@norkart.no>
2023-02-14 20:54:55 +0100

    Can shoot

--- 2DShooter/2DShooter.cpp
+++ 2DShooter/2DShooter.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #define PLAYER_HOR_SPD 350.0f
+#define PLAYER_MAX_SHOTS 5
 
 typedef struct PlanePlayer
 {
@@ -12,10 +13,37 @@ typedef struct PlanePlayer
     Texture2D planeTexture;
 } PlanePlayer;
 
+typedef struct BulletPlayer
+{
+    Vector2 position;
+    float speed;
+    float radius;
+    bool active;
+    Color color;
+    float lifespawn;
+} BulletPlayer;
+
+typedef struct BulletPlayerRight
+{
+    Vector2 position;
+    float speed;
+    float radius;
+    bool active;
+    Color color;
+    float lifespawn;
+} BulletPlayerRight;
+
 void UpdatePlane(PlanePlayer *player, float delta, int width, int height);
+void UpdateLeftBullet(BulletPlayer (*bullet)[PLAYER_MAX_SHOTS], Vector2 position, float delta);
+void UpdateRightBullet(BulletPlayerRight (*bullet)[PLAYER_MAX_SHOTS], Vector2 position, float delta);
 
 int main(void)
 {
+    static BulletPlayer bullet[PLAYER_MAX_SHOTS] = {0};
+    static BulletPlayerRight bulletRight[PLAYER_MAX_SHOTS] = {0};
+
+    bool left;
+    bool right;
 
     float screenWidth = 1024.f;
     float screenHeight = 1024.f;
@@ -23,19 +51,43 @@ int main(void)
 
     Image background = LoadImage("../mymedia/2d_desert_sprite.png");
     ImageResize(&background, background.width, background.height);
+    Texture2D texture = LoadTextureFromImage(background);
+    UnloadImage(background);
 
-    Image plane = LoadImage("../mymedia/plane_100_0.png");
+    Image planeImg = LoadImage("../mymedia/plane_100_0.png");
     Vector2 planePosition = {screenWidth / 2, screenHeight / 2};
     PlanePlayer planePlayer = {0};
-    planePlayer.planeTexture = LoadTextureFromImage(plane);
+    planePlayer.planeTexture = LoadTextureFromImage(planeImg);
     planePlayer.position = planePosition;
     planePlayer.speed = 8.f;
     planePlayer.canShoot = true;
-    UnloadImage(plane);
+    UnloadImage(planeImg);
 
-    Texture2D texture = LoadTextureFromImage(background);
+    // Starting positions of bullets
+    Vector2 bulletStartPos = {planePosition.x + 16, planePosition.y + 4};
+    Vector2 bulletRightStartPos = {planePosition.x + 80, planePosition.y + 4};
+    //
 
-    UnloadImage(background);
+    for (int i = 0; i < PLAYER_MAX_SHOTS; i++)
+    {
+        Vector2 bulletPosition = {bulletStartPos.x, bulletStartPos.y - 20};
+        bullet[i].position = bulletPosition;
+        bullet[i].speed = 1700.f;
+        bullet[i].radius = 10;
+        bullet[i].color = RED;
+        bullet[i].active = false;
+        bullet[i].lifespawn = 0;
+    }
+
+    for (int i = 0; i < PLAYER_MAX_SHOTS; i++)
+    {
+        Vector2 bulletPosition = {bulletRightStartPos.x, bulletRightStartPos.y - 20};
+        bulletRight[i].speed = 1700.f;
+        bulletRight[i].radius = 10;
+        bulletRight[i].color = RED;
+        bulletRight[i].active = false;
+        bulletRight[i].lifespawn = 0;
+    }
 
     SetTargetFPS(60);
 
@@ -51,6 +103,8 @@ int main(void)
         DrawTexture(texture, 0, 0, RAYWHITE);
         DrawTexture(planePlayer.planeTexture, planePlayer.position.x, planePlayer.position.y, WHITE);
 
+        UpdateLeftBullet(&bullet, planePlayer.position, deltaTime);
+        UpdateRightBullet(&bulletRight, planePlayer.position, deltaTime);
         // DrawText("Dette er ett spill", screenWidth / 2, screenHeight / 2, 45, GREEN);
         EndDrawing();
     }
@@ -71,4 +125,60 @@ void UpdatePlane(PlanePlayer *player, float delta, int width, int height)
         player->position.y += PLAYER_HOR_SPD * delta;
     if (IsKeyDown(KEY_UP) && player->position.y >= 0)
         player->position.y -= PLAYER_HOR_SPD * delta;
+}
+
+void UpdateLeftBullet(BulletPlayer (*bullet)[PLAYER_MAX_SHOTS], Vector2 position, float delta)
+{
+
+    for (int i = 0; i < PLAYER_MAX_SHOTS; i++)
+    {
+        if (!bullet[i]->active)
+        {
+            bullet[i]->position = Vector2{position.x + 16, position.y};
+            bullet[i]->active = true;
+            break;
+        }
+    }
+
+    for (int i = 0; i < PLAYER_MAX_SHOTS; i++)
+    {
+        if (bullet[i]->active)
+        {
+            bullet[i]->position.y -= bullet[i]->speed * delta;
+
+            if (bullet[i]->position.y < 0)
+                bullet[i]->active = false;
+
+            if (bullet[i]->active)
+                DrawRectangle(bullet[i]->position.x, bullet[i]->position.y, 5, 20, bullet[i]->color);
+        }
+    }
+}
+
+void UpdateRightBullet(BulletPlayerRight (*bullet)[PLAYER_MAX_SHOTS], Vector2 position, float delta)
+{
+
+    for (int i = 0; i < PLAYER_MAX_SHOTS; i++)
+    {
+        if (!bullet[i]->active)
+        {
+            bullet[i]->position = Vector2{position.x + 80, position.y};
+            bullet[i]->active = true;
+            break;
+        }
+    }
+
+    for (int i = 0; i < PLAYER_MAX_SHOTS; i++)
+    {
+        if (bullet[i]->active)
+        {
+            bullet[i]->position.y -= bullet[i]->speed * delta;
+
+            if (bullet[i]->position.y < 0)
+                bullet[i]->active = false;
+
+            if (bullet[i]->active)
+                DrawRectangle(bullet[i]->position.x, bullet[i]->position.y, 5, 20, bullet[i]->color);
+        }
+    }
 }
