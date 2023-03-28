6111e03eb5e410758d8c742c94171a043fb50127
scalerow <jf.sundblom@outlook.com>
2023-03-28 17:36:29 +0200

    Begun setup off boss and some more structure to the game * Level specific events * Spawning boss functions

---
+++ 2DShooter/levels/level_01.cpp
@@ -0,0 +1,18 @@
+#include "level_01.h"
+
+void SpawnBoss()
+{
+    bossEnemy.InitBoss(CalculateXCoord(100/2), 500, 2);
+
+}
+
+void UpdateBoss() 
+{
+
+}
+
+void isBossHit()
+{}
+
+void LevelComplete()
+{}
